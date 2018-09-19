aae174b44a276765e29c2d896681f197ba3c7866
Diego Caroli <diegocaroli+dtt@gmail.com>
2018-09-19 10:12:49 +0200

    Initial Commit

---
+++ TrimmerVideo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrimmerVideo
+//
+//  Created by Diego Caroli on 19/09/2018.
+//  Copyright © 2018 Diego Caroli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
