ebaf07a5a384c367cb79970a9f6257d84fdc0e52
Diego Caroli <diegocaroli+dtt@gmail.com>
2018-09-19 23:46:22 +0200

    added mask view

--- TrimmerVideo/TrimmerView.swift
+++ TrimmerVideo/TrimmerView.swift
@@ -12,19 +12,22 @@ import UIKit
 @IBDesignable
 class TrimmerView: UIView {
   
-  @IBInspectable var mainColor: UIColor = UIColor.black
-  @IBInspectable var handColor: UIColor = UIColor.black
+  @IBInspectable var mainColor: UIColor = UIColor.black {
+    didSet {
+      
+    }
+  }
   
-  private let borderView: UIView = {
+  private let trimView: UIView = {
     let view = UIView()
     view.frame = .zero
     view.backgroundColor = .clear
+    view.layer.borderWidth = 2
+    view.layer.borderColor = UIColor.orange.cgColor
     return view
   }()
-  var borderWidth: CGFloat = 20
   
-  
-  private let leftPanView: UIView = {
+  private let leftDraggableView: UIView = {
     let view = UIView()
     view.frame = .zero
     view.backgroundColor = .orange
@@ -32,17 +35,41 @@ class TrimmerView: UIView {
     return view
   }()
   
-  private let rightPanView: UIView = {
+  private let rightDraggableView: UIView = {
     let view = UIView()
     view.frame = .zero
     view.backgroundColor = .orange
     view.translatesAutoresizingMaskIntoConstraints = false
     return view
   }()
+  
+  private let leftMaskView: UIView = {
+    let view = UIView()
+    view.frame = .zero
+    view.backgroundColor = .white
+    view.alpha = 0.7
+    view.translatesAutoresizingMaskIntoConstraints = false
+    view.isUserInteractionEnabled = false
+    return view
+  }()
+  
+  private let rightMaskView: UIView = {
+    let view = UIView()
+    view.frame = .zero
+    view.backgroundColor = .white
+    view.alpha = 0.7
+    view.translatesAutoresizingMaskIntoConstraints = false
+    view.isUserInteractionEnabled = false
+    return view
+  }()
+  
+  private let draggableViewWidth: CGFloat = 15
   
   let dimmingView = DimmingView()
   var leadingConstraint: NSLayoutConstraint?
   var trailingConstraint: NSLayoutConstraint?
+  
+  
   
   override func awakeFromNib() {
     super.awakeFromNib()
@@ -54,54 +81,104 @@ class TrimmerView: UIView {
     super.layoutSubviews()
     
     dimmingView.frame = bounds
-    borderView.frame = dimmingView.bounds
+    trimView.frame = dimmingView.bounds
     
-    leadingConstraint = leftPanView.leadingAnchor.constraint(equalTo: borderView.leadingAnchor, constant: 0)
-    trailingConstraint = rightPanView.trailingAnchor.constraint(equalTo: borderView.trailingAnchor, constant: 0)
+    leadingConstraint = leftDraggableView.leadingAnchor.constraint(equalTo: trimView.leadingAnchor, constant: 0)
+    trailingConstraint = rightDraggableView.trailingAnchor.constraint(equalTo: trimView.trailingAnchor, constant: 0)
     
     NSLayoutConstraint.activate([
       leadingConstraint!,
-      leftPanView.topAnchor.constraint(equalTo: borderView.topAnchor, constant: 0),
-      leftPanView.bottomAnchor.constraint(equalTo: borderView.bottomAnchor, constant: 0),
-      leftPanView.widthAnchor.constraint(equalToConstant: borderWidth),
-      rightPanView.topAnchor.constraint(equalTo: borderView.topAnchor, constant: 0),
-      rightPanView.bottomAnchor.constraint(equalTo: borderView.bottomAnchor, constant: 0),
-      rightPanView.widthAnchor.constraint(equalToConstant: borderWidth),
-      trailingConstraint!
+      leftDraggableView.topAnchor.constraint(equalTo: trimView.topAnchor, constant: 0),
+      leftDraggableView.bottomAnchor.constraint(equalTo: trimView.bottomAnchor, constant: 0),
+      leftDraggableView.widthAnchor.constraint(equalToConstant: draggableViewWidth),
+      rightDraggableView.topAnchor.constraint(equalTo: trimView.topAnchor, constant: 0),
+      rightDraggableView.bottomAnchor.constraint(equalTo: trimView.bottomAnchor, constant: 0),
+      rightDraggableView.widthAnchor.constraint(equalToConstant: draggableViewWidth),
+      trailingConstraint!,
+      
+      leftMaskView.topAnchor.constraint(equalTo: trimView.topAnchor, constant: 0),
+      leftMaskView.bottomAnchor.constraint(equalTo: trimView.bottomAnchor, constant: 0),
+      leftMaskView.leadingAnchor.constraint(equalTo: trimView.leadingAnchor, constant: 0),
+      leftMaskView.trailingAnchor.constraint(equalTo: leftDraggableView.leadingAnchor, constant: 0)
       ])
   }
   
   private func setup() {
     addSubview(dimmingView)
-    addSubview(borderView)
-    borderView.addSubview(leftPanView)
-    borderView.addSubview(rightPanView)
-    
-    borderView.layer.borderWidth = 2
-    borderView.layer.borderColor = UIColor.orange.cgColor
+    addSubview(trimView)
+    trimView.addSubview(leftDraggableView)
+    trimView.addSubview(rightDraggableView)
+    trimView.addSubview(leftMaskView)
     
     setupPanGestures()
   }
   
   private func setupPanGestures() {
     let leftPanGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
-    leftPanView.addGestureRecognizer(leftPanGesture)
+    leftDraggableView.addGestureRecognizer(leftPanGesture)
     let rightPanGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
-    rightPanView.addGestureRecognizer(rightPanGesture)
+    rightDraggableView.addGestureRecognizer(rightPanGesture)
   }
   
   
   //fix me
   @objc func handlePan(_ sender: UIPanGestureRecognizer) {
-    let translation = sender.translation(in: sender.view)
+    guard let view = sender.view else { return }
     
-    print(rightPanView.frame)
-    if translation.x >= 0 &&
-      translation.x <= rightPanView.frame.minX {
-      leadingConstraint?.constant += translation.x
+    let isLeftGesture = view == leftDraggableView
+    switch sender.state {
+      
+//    case .began:
+//      if isLeftGesture {
+//        currentLeftConstraint = leftConstraint!.constant
+//      } else {
+//        currentRightConstraint = rightConstraint!.constant
+//      }
+//      updateSelectedTime(stoppedMoving: false)
+    case .changed:
+      let translation = sender.translation(in: view)
+      if isLeftGesture {
+        updateLeadingConstraint(with: translation)
+      }
+//      else {
+//        updateRightConstraint(with: translation)
+//      }
+      layoutIfNeeded()
+//      if let startTime = startTime, isLeftGesture {
+//        seek(to: startTime)
+//      } else if let endTime = endTime {
+//        seek(to: endTime)
+//      }
+//      updateSelectedTime(stoppedMoving: false)
+      
+//    case .cancelled, .ended, .failed:
+//      updateSelectedTime(stoppedMoving: true)
+    default: break
     }
-    
-    sender.setTranslation(.zero, in: sender.view)
   }
+  
+  private func updateLeadingConstraint(with translation: CGPoint) {
+    let maxConstraint = max(rightDraggableView.frame.origin.x - draggableViewWidth - minimumDistanceBetweenDraggableViews, 0)
+    let newConstraint = max(min(translation.x, maxConstraint), 0)
+    
+    CATransaction.begin()
+    CATransaction.setDisableActions(true)
+    leadingConstraint?.constant = newConstraint
+    CATransaction.commit()
+    
+    print(translation, maxConstraint, newConstraint)
+  }
+  
+  private var minimumDistanceBetweenDraggableViews: CGFloat {
+    return CGFloat(2) * (dimmingView.frame.width / CGFloat(dimmingView.asset.duration.seconds))
+  }
+  
+  var absolutePosition: CGFloat = 0
+  
+  // Normalized
+  var relativePosition: CGFloat {
+    get { return absolutePosition / dimmingView.bounds.width }
+    set { absolutePosition = newValue }
+    }
 
 }
